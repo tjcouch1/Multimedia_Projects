23e75b5e550b3f29da60e9fe5fc3aaffb9d017a2
tjcouch <tjcouch1@gmail.com>
2019-01-29 16:29:33 -0600

    Potentially finished orderedDither, ditherGIF doesn't write, though

--- Couch_HW1/ditherGIF.m
+++ Couch_HW1/ditherGIF.m
@@ -5,3 +5,27 @@
 % Homework 1
 % CS 443 Multimedia
 % 1/29/19
+
+outImageName = 'dithered_animation.gif';
+
+%apply dither to each of 50 images
+for i = 1:1%50
+    fileName = sprintf("homework1_animation_frames/frame_%d_delay-0.1s.gif", i);
+    
+    %read image and convert from byte indexed to rgb
+    [indexedSrcImg, colorMap] = imread(fileName, "Frames", 1);
+    sourceImg = ind2rgb(indexedSrcImg, colorMap);
+    
+    %apply dither to image
+    ditheredImg = orderedDither(sourceImg);
+    
+    %stitch image into the gif
+    %format image into byte indexed image
+    [indexedImg, colorMap] = rgb2ind(ditheredImg,256);
+    %write indexed image to the file
+    if n == 1
+      imwrite(indexedImg, colorMap, outImageName, 'gif', 'Loopcount', inf);
+    else
+      imwrite(indexedImg, colorMap, outImageName, 'gif', 'WriteMode', 'append');
+    end
+end
--- Couch_HW1/orderedDither.m
+++ Couch_HW1/orderedDither.m
@@ -1,22 +1,41 @@
-% orderedDither.m - ordered dithers the file provided and writes it with _d
-% appended to the name
-% orderedDither(fileName)
-% fileName: String path to image to dither
-% returns true if file written, false otherwise
+% orderedDither.m - ordered dithers the image matrix provided and writes it
+% with _d appended to the name
+% orderedDither(image)
+% image: the rgb image matrix to dither
+% Note: the image must be in rgb format
+% returns dithered image
 % 
 % TJ Couch
 % Homework 1
 % CS 443 Multimedia
 % 1/24/19
 
-function f = orderedDither(fileName)
+function f = orderedDither(sourceImg)
 
-sourceImg = imread(fileName);
-imshow(sourceImg)
+%set up dither matrix
+n = 4;%size of dither matrix - 4x4
+ditherMatrix = (1/16) .* [0 8 2 10; 12 4 14 6; 3 11 1 9; 15 7 13 5];
 
-f = true;
+%convert to grayscale
+grayImg = rgb2gray(sourceImg);
 
-%imread()
-%imshow()
-%imwrite()
-%rgb2gray()
+%make new image matrix of the proper size
+[rows, cols] = size(grayImg)
+ditheredImg = zeros(rows, cols);
+
+%loop through each pixel and replace with ordered dithered pixel
+for i = 1:rows
+    for j = 1:cols
+        %get value of pixel
+        val = grayImg(i, j);
+        
+        %set pixel to white if appropriate
+        if val >= ditherMatrix(mod(i - 1, n) + 1, mod(j - 1, n) + 1)
+            ditheredImg(i, j) = 1;
+        end
+    end
+end
+
+imshow(ditheredImg)
+
+f = ditheredImg;
